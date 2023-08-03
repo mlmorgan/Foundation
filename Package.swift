4c9880666f26880f3b963fff5d7ecafa8812b56f
nieys <40758182+nieys@users.noreply.github.com>
2023-08-03 15:54:13 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@ import PackageDescription
 let package = Package(
     name: "PMKFoundation",
     dependencies: [
-        .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 6)
+        .Package(url: "https://github.com/mxcl/PromiseKit.git", majorVersion: 8)
     ],
-    swiftLanguageVersions: [3, 4],
+    swiftLanguageVersions: [3, 4, 5],
     exclude: [
         "Sources/NSNotificationCenter+AnyPromise.m",
         "Sources/NSTask+AnyPromise.m",
